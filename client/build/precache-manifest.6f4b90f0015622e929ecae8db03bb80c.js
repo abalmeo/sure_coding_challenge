489a68ba37306ee2a0357a4e76825edb96a09508
Alvin Balmeo <albalmeo@gmail.com>
2019-09-05 23:03:29 -0700

    Building application to host

---
+++ client/build/precache-manifest.6f4b90f0015622e929ecae8db03bb80c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41203b631f2a173904ce11a97f99df0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88c082b7e14912a68524",
+    "url": "/static/css/main.ed551938.chunk.css"
+  },
+  {
+    "revision": "3748c2e9d59e3f88c85b",
+    "url": "/static/js/2.5fb1aa3e.chunk.js"
+  },
+  {
+    "revision": "88c082b7e14912a68524",
+    "url": "/static/js/main.e4426687.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
